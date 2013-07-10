0fc5d19be90e581e1ef8276e847ca27a2fd70113
Ramesh Vagesan <vramesh.1987@gmail.com>
2013-07-10 18:56:56 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var infile = "index.html";
+  response.send(fs.readFileSync(infile).toString());
 });
 
 var port = process.env.PORT || 5000;
